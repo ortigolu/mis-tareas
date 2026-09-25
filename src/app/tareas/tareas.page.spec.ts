313cea3d5ebe66132906f07996b6768a9bbea973
ortigolu <vpasuenolar@gmail.com>
2026-09-25 19:21:35 -0300

    first commit

--- src/app/tareas/tareas.page.spec.ts
+++ src/app/tareas/tareas.page.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+aimport { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TareasPage } from './tareas.page';
 
 describe('TareasPage', () => {
